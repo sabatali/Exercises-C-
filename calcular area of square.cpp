4577bae3b1eeab6446102037607747744b1a59eb
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-03-12 17:06:04 +0500

    calcular area of square

---
+++ calcular area of square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int width ;
+	int Length;
+	int area;
+	
+	cout << "Enter Width : "; 
+	cin>> width;
+	cout << "Enter Length : ";
+	cin>> Length;
+	
+	area = width * Length;
+	
+	cout << "area of square "<< area ;
+	
+}
