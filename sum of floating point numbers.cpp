153bab3d194736ca689cf473670905e1ff3e2ec5
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-03-12 17:18:17 +0500

    sum of floating point numbers

---
+++ sum of floating point numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	float num1 ;
+	float num2;
+	float sum;
+	
+	cout << "Enter number1 : "; 
+	cin>> num1;
+	cout << "Enter number2 : ";
+	cin>> num2;
+	
+	sum = num1 + num2;
+	
+	cout << "sum of floating point numbers "<< sum ;
+	
+}
